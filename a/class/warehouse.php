2bff284ce84eb208c1aedc6aead4dcdf58d40a02
SergNikolaev <iintersergey@gmail.com>
2015-05-27 18:06:28 +0500

    2 day develop end

--- a/class/warehouse.php
+++ a/class/warehouse.php
@@ -3,7 +3,22 @@
 class warehouse extends parent_protect{
 
     public function add(){
-        $_POST['user_id'] = $this->user['id'];
-        $this->db->insert('warehouse', $_POST);
+        $this->ifPost();
+        $data = array(
+            'user_id'=>$this->user['id'],
+            'name'=>$_POST['name'],
+            'count'=>$_POST['count'],
+            'item'=>$_POST['item'],
+            'price'=>$_POST['price'],
+            'per'=>$_POST['per']
+        );
+        if($this->db->insert('warehouse', $data)){
+            $this->Success(array('m'=>'Данные добавлены'));
+        }else{$this->Error(array('m'=>'Ошибка добавления данных'));}
+
+    }
+    public function getAll(){
+        $this->db->where('user_id',$this->user['id']);
+        $this->Success($this->db->get('warehouse'));
     }
 } 
